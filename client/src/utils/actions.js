4c242b76ec451dbf7481ea829917694fb8a05adc
kfisch2 <kmlindsey24@gmail.com>
2022-09-26 14:24:16 -0700

    tests for reducer functions

---
+++ client/src/utils/actions.js
@@ -0,0 +1,5 @@
+// defining how these three parts of our state will be maintained
+export const UPDATE_PRODUCTS = "UPDATE_PRODUCTS";
+export const UPDATE_CATEGORIES = "UPDATE_CATEGORIES";
+export const UPDATE_CURRENT_CATEGORY = "UPDATE_CURRENT_CATEGORY";
+
