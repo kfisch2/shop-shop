b4406da6ac108c584d59c25106ae462d3e7730aa
kfisch2 <kmlindsey24@gmail.com>
2022-09-27 16:16:01 -0700

    add tests for cart

--- client/src/__tests__/reducers.test.js
+++ client/src/__tests__/reducers.test.js
@@ -4,7 +4,13 @@ import {
   UPDATE_PRODUCTS,
   UPDATE_CATEGORIES,
   UPDATE_CURRENT_CATEGORY,
-} from "../../../server/utils/actions";
+  ADD_TO_CART,
+  ADD_MULTIPLE_TO_CART,
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  CLEAR_CART,
+  TOGGLE_CART
+} from '../utils/actions';
 
 // import reducer
 import { reducer } from "../../../server/utils/reducers";
