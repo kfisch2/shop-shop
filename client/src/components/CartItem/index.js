7ba1eea9633a0e4c5ac1a7da1fed643eb45eaeeb
kfisch2 <kmlindsey24@gmail.com>
2022-09-29 16:33:30 -0700

    cart persists if user is not connected to server

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,33 +1,38 @@
 import React from "react";
-import { useStoreContext } from '../../utils/GlobalState';
-import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions'
+import { useStoreContext } from "../../utils/GlobalState";
+import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
+import { idbPromise } from "../../utils/helpers";
 
 const CartItem = ({ item }) => {
   const [, dispatch] = useStoreContext();
 
-  const removeFromCart = item => {
+  const removeFromCart = (item) => {
     dispatch({
       type: REMOVE_FROM_CART,
-      _id: item._id
-    })
-  }
+      _id: item._id,
+    });
+    idbPromise("cart", "delete", { ...item });
+  };
 
   const onChage = (e) => {
     const value = e.target.value;
 
-    if (value === '0') {
+    if (value === "0") {
       dispatch({
         type: REMOVE_FROM_CART,
-        _id: item._id
-      })
+        _id: item._id,
+      });
+
+      idbPromise("cart", "delete", { ...item });
     } else {
       dispatch({
         type: UPDATE_CART_QUANTITY,
         _id: item._id,
-        purchaseQuantity: parseInt(value)
-      })
+        purchaseQuantity: parseInt(value),
+      });
+      idbPromise("cart", "put", { ...item, purchaseQuantity: parseInt(value) });
     }
-  }
+  };
   return (
     <div className="flex-row">
       <div>
@@ -40,8 +45,19 @@ const CartItem = ({ item }) => {
         <div>
           <span>Qty: </span>
           {/* without an onChange hanlder, the input will be a 'readonly' */}
-          <input type="number" placeholder="1" value={item.purchaseQuantity} onChange={onChage} />
-          <span role="img" aria-label="trash" onClick={() => removeFromCart(item)}>🗑️</span>
+          <input
+            type="number"
+            placeholder="1"
+            value={item.purchaseQuantity}
+            onChange={onChage}
+          />
+          <span
+            role="img"
+            aria-label="trash"
+            onClick={() => removeFromCart(item)}
+          >
+            🗑️
+          </span>
         </div>
       </div>
     </div>
